440a0b974f38af8afbeeef6bf6b3ee272bd140cb
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-09-26 19:52:22 +0300

    Adds Google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins, Butcherman } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--body-font'
+})
+const butcherman = Butcherman({ 
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+  variable: '--second-font'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${poppins.variable} ${butcherman.variable}`}>
+      <body>{children}</body>
     </html>
   )
 }
