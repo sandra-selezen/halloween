2ebf6f271cdcd20ed19a07a38297afa44e804032
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-09-27 19:11:06 +0300

    Adds styles for Home section

--- sections/Home/Home.tsx
+++ sections/Home/Home.tsx
@@ -12,14 +12,15 @@ export const Home = () => {
         <div className='home__data'>
           <h1 className='home__title'>
             <span>Halloween</span> Trick Or Treat!!!
-            <Image src={StarsImg} alt='stars' className='home__title-img-1' />
-            <Image src={StarsImg} alt='stars' className='home__title-img-2' />
+            <Image src={StarsImg} alt='stars' className='home__title-img-1' width={40} />
+            <Image src={StarsImg} alt='stars' className='home__title-img-2' width={40} />
           </h1>
           <p className='home__description'>This Halloween, enjoy trick-or-treating with your friends and spend a terrifying night under the full moon.</p>
           <Link href={'#'} className='button'>Explore Now!!!</Link>
         </div>
-        <Image src={Tree1} alt='tree' className='home__tree-1' />
-        <Image src={Tree2} alt='tree' className='home__tree-2' />
+        <Image src={PumpkinImg} alt='pumpkin' className='home__img' width={300} />
+        <Image src={Tree1} alt='tree' className='home__tree-1' width={120} />
+        <Image src={Tree2} alt='tree' className='home__tree-2' width={120} />
       </div>
     </section>
   )
